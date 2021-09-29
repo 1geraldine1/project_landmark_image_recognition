c5c26f28ead9f5ebc6c707bf9c6bf1055bcc7740
KimJinWoo <1geraldine1@naver.com>
2021-09-29 17:51:44 +0900

    20210929 프로젝트 1차 완성

--- landmark/urls.py
+++ landmark/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
 	path('',index,name="index"),
 	path('result',result,name="result"),
-	path('recommand',recommand,name="recommand")
+	path('recommand',recommand,name="recommand"),
+	path('recommand_new',recommand_new,name='recommand_new')
 
 ]
